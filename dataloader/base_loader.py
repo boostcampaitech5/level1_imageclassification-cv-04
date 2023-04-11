8ecf7f5c83ab5631a0bc9f57b23036f38617de40
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 11:53:55 +0900

    baseloader 구현

---
+++ dataloader/base_loader.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+from abc import *
+import pathlib
+class BaseLoader(Dataset, metaclass=ABCMeta):
+    def __init__(self, base_dir):
+        print('Data dir: ',base_dir)
+        self.base_dir = pathlib.Path(base_dir)
+        self.data_list = self._get_file_list()
+    def __len__(self):
+        return len(self.data)
+    
+    @abstractmethod
+    def __getitem__(self,idx):
+        raise NotImplementedError
+    @abstractmethod
+    def _get_file_list(self):
+        raise NotImplementedError
+
