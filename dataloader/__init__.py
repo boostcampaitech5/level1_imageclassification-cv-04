50c8d70f3e01e78bb8c64a8f9fdd1e702f78929e
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 11:54:15 +0900

    dataloader 동적할당 구현

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -8,8 +8,7 @@ def get_loader(loader_name, args):
     target_lib = 'dataloader.'+loader_name
     print("Target Module: ",target_lib)
     loader_lib = importlib.import_module(target_lib)
-    target_loader = "".join(map(str.title,target_lib.split('_')))
+    target_loader = ''.join(map(str.title,loader_name.split('_')))
     print("Target Loader: ",target_loader)
-    for name,cls in loader_lib.__dict__:
-        print(name,cls)
+    getattr(loader_lib,target_loader)('data')
 
