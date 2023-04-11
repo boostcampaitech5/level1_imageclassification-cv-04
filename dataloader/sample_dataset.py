3532d097d42eaee519b9971af7f762dd4c9b1269
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 15:27:37 +0900

    dataloader구현 수정

--- dataloader/sample_dataset.py
+++ dataloader/sample_dataset.py
@@ -1,10 +1,10 @@
-from dataloader.base_loader import BaseLoader
+from dataloader.base_dataset import BaseDataset
 import pathlib
 
-class SampleLoader(BaseLoader):
+class SampleDataset(BaseDataset):
     def __init__(self,base_dir):
         super().__init__(base_dir)
     def __getitem__(self, idx):
         print('get item')
     def _get_file_list(self):
-        print('get_file_list')
+        self.data = list(self.base_dir.glob('*'))
