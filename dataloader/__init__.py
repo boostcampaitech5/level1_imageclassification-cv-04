23294b48ce72a3463b75a6e8f7a4bc35d1a4a9c4
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 12:19:09 +0900

    get_loader로그 추가

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -5,10 +5,11 @@ import importlib
 
 
 def get_loader(loader_name, args):
+    print('Load Dataloader start')
     target_lib = 'dataloader.'+loader_name
     print("Target Module: ",target_lib)
     loader_lib = importlib.import_module(target_lib)
     target_loader = ''.join(map(str.title,loader_name.split('_')))
     print("Target Loader: ",target_loader)
-    getattr(loader_lib,target_loader)(**args)
+    return getattr(loader_lib,target_loader)(**args)
 
