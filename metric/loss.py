316e6475e3dee8c7d91ee6429bb7e7949c702b90
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 20:02:50 +0900

    set_loss 추가

--- metric/loss.py
+++ metric/loss.py
@@ -1,5 +1,12 @@
+from torch import nn
+
+
 def topk_loss(logit, y, criterion, k=5):
     '''
     criterion에 따른 k개의 top loss index를 반환합니다.
     '''
-    
+    pass
+def set_loss(target_loss):
+    loss_list = {"BCELoss":nn.BCELoss, "CrossEntropyLoss":nn.CrossEntropyLoss,
+                 "MSELoss":nn.MSELoss}
+    return loss_list[target_loss]()
