14433bbd5517d117b358da41c4f78f54540e64ad
zionia4758 <2016025196@hanyang.ac.kr>
2023-04-11 11:54:05 +0900

    sample dataloader 구현

--- dataloader/sample_loader.py
+++ dataloader/sample_loader.py
@@ -1,1 +1,10 @@
-print(2)
+from dataloader.base_loader import BaseLoader
+import pathlib
+
+class SampleLoader(BaseLoader):
+    def __init__(self,base_dir):
+        super().__init__(base_dir)
+    def __getitem__(self, idx):
+        print('get item')
+    def _get_file_list(self):
+        print('get_file_list')
