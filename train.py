e565170c64b744f699cfd3b5d118f089b1fcaf76
jjjuuuun <kjy961226@naver.com>
2023-04-12 12:09:39 +0000

    초기 설정 완료 - 모델 만들고 transformer 만들고 넣으면 됨

--- train.py
+++ train.py
@@ -24,44 +24,44 @@ def torch_seed(random_seed):
     random.seed(random_seed)
     os.environ['PYTHONHASHSEED'] = str(random_seed)
 
-def run(cfg):
-    if cfg.use_wandb:
+def run(args, args_dict):
+    if args.use_wandb:
         print('Initialize WandB ...')
-        wandb.init(name = cfg.wandb_exp_name,
-                   project = cfg.wandb_project_name,
-                   entity = cfg.wandb_entity,
-                   config = cfg)
+        wandb.init(name = args.wandb_exp_name,
+                   project = args.wandb_project_name,
+                   entity = args.wandb_entity,
+                   config = args_dict)
         
-    torch_seed(cfg.seed)
+    torch_seed(args.seed)
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'The device is ready\t>>\t{device}')
 
     transform = transforms.Compose([transforms.Resize((256, 256)),
                                     transforms.ToTensor()])
 
-    dataset = ClassificationDataset(csv_path = cfg.csv_path,
+    dataset = ClassificationDataset(csv_path = args.csv_path,
                                     transform=transform,
-                                    num_classes = cfg.num_classes)
+                                    num_classes = args.num_classes)
     print(f'The number of training images\t>>\t{len(dataset)}')
 
     train_iter = DataLoader(dataset,
-                            batch_size=cfg.batch_size,
+                            batch_size=args.batch_size,
                             shuffle = True)
     
 
     print('The model is ready ...')
-    model = Network(num_classes = cfg.num_classes).to(device)
-    if cfg.model_summary:
+    model = Network(num_classes = args.num_classes).to(device)
+    if args.model_summary:
         print(summary(model, (3, 256, 256)))
 
     print('The optimizer is ready ...')
-    optimizer = optim.Adam(params=model.parameters(), lr=cfg.learning_rate)
+    optimizer = optim.Adam(params=model.parameters(), lr=args.learning_rate)
 
     print('The loss function is ready ...')
     criterion = nn.CrossEntropyLoss()
 
     print("Starting training ...")
-    for epoch in range(cfg.epochs):
+    for epoch in range(args.epochs):
         start_time = time.time()
         train_epoch_loss = 0
         model.train()
@@ -79,10 +79,10 @@ def run(cfg):
 
         train_epoch_loss = train_epoch_loss / len(train_iter)
 
-        cm = confusion_matrix(model, train_iter, device, cfg.num_classes)
+        cm = confusion_matrix(model, train_iter, device, args.num_classes)
 
-        train_acc = accuracy(cm, cfg.num_classes)
-        train_f1 = f1_score(cm, cfg.num_classes)
+        train_acc = accuracy(cm, args.num_classes)
+        train_f1 = f1_score(cm, args.num_classes)
 
         print('time >> {:.4f}\tepoch >> {:04d}\ttrain_acc >> {:.4f}\ttrain_loss >> {:.4f}\ttrain_f1 >> {:.4f}'
               .format(time.time()-start_time, epoch, train_acc, train_epoch_loss, train_f1))
@@ -92,27 +92,31 @@ def run(cfg):
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
-                }, os.path.join(cfg.save_path, f'epoch({epoch})_acc({train_acc:.3f})_loss({train_epoch_loss:.3f})_f1({train_f1:.3f}).pt'))
+                }, os.path.join(args.save_path, f'epoch({epoch})_acc({train_acc:.3f})_loss({train_epoch_loss:.3f})_f1({train_f1:.3f}).pt'))
         
-        if cfg.use_wandb:
-            wandb.log({'Train_Acc': train_acc,
-                       'Train_Loss': train_epoch_loss,
-                       'Train_F1_Score': train_f1})
+        if args.use_wandb:
+            wandb.log({'Train Acc': train_acc,
+                       'Train Loss': train_epoch_loss,
+                       'Train F1-Score': train_f1})
 
 
 if __name__ == '__main__':
-    class cfg:
-        seed = 223
-        csv_path = './input/data/train/train_info.csv'
-        save_path = './checkpoint'
-        use_wandb = True
-        wandb_exp_name = 'test'
-        wandb_project_name = 'Image_classification_mask'
-        wandb_entity = 'connect-cv-04'
-        num_classes = 18
-        model_summary = False
-        batch_size = 128
-        learning_rate = 1e-4
-        epochs = 1
+    args_dict = {'seed' : 223,
+                 'csv_path' : './input/data/train/train_info.csv',
+                 'save_path' : './checkpoint',
+                 'use_wandb' : True,
+                 'wandb_exp_name' : 'test',
+                 'wandb_project_name' : 'Image_classification_mask',
+                 'wandb_entity' : 'connect-cv-04',
+                 'num_classes' : 18,
+                 'model_summary' : False,
+                 'batch_size' : 128,
+                 'learning_rate' : 1e-4,
+                 'epochs' : 1}
+    
+    from collections import namedtuple
+    Args = namedtuple('Args', args_dict.keys())
+    args = Args(**args_dict)
 
-    run(cfg)
+    # Config parser 하나만 넣어주면 됨(임시방편)
+    run(args, args_dict)
