3abb3364049f861adc9abfa7773f2ee1785e4951
jjjuuuun <kjy961226@naver.com>
2023-04-12 03:30:00 +0000

    dataset.py 완성

---
+++ dataloader/dataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms 
+import pandas as pd
+from PIL import Image
+
+class ClassificationDataset(Dataset):
+    """Data loader를 만들기 위한 base dataset class"""
+
+    def __init__(self, csv_path, transform=None, num_classes=-1):
+        self.df = pd.read_csv(csv_path)
+        self.transform = transform
+        self.num_classes = num_classes
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.df.iloc[idx].ImageID)
+        label = self.df.iloc[idx].ans
+
+        if self.transform:
+            img = self.transform(img)
+
+        one_hot_label = torch.zeros((self.num_classes,))
+        one_hot_label[label] = 1
+            
+        return img, one_hot_label
+    
+if __name__ == '__main__':
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    dataset = ClassificationDataset(csv_path = './input/data/train/train_info.csv',
+                                    transform=transform,
+                                    num_classes = 18)
+    data_iter = DataLoader(dataset,
+                           batch_size=3,
+                           shuffle = True)
+    
+    print(next(iter(data_iter)))
