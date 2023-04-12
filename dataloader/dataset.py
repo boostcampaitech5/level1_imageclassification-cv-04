8a866df1dea14b6acb1909e2bc5bd2a65a3f4b93
Junha <jh58power@naver.com>
2023-04-12 06:19:50 +0000

    Dataset 끝

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,6 +1,7 @@
 
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+from torch.utils import data
 import pandas as pd
 from PIL import Image
 import os
@@ -8,14 +9,14 @@ import csv
 
 #Mask Image Classification 마스크 착용 여부
 class IC_Dataset(Dataset): 
-    def __init__(self,img_dir, trans_list = None):
+    def __init__(self,img_dir, trans_list = [transforms.ToTensor()]):
         self.dataset_dir = img_dir
         self.transform = trans_list
         
         
-        self.files = self.getXY(self.dataset_dir)
-        
-        
+        x,y = self.getXY(self.dataset_dir)
+        self.X = x
+        self.Y = y
         
     def __len__(self):
         return len(self.X)
@@ -23,25 +24,28 @@ class IC_Dataset(Dataset):
     def __getitem__(self,idx):
         X = Image.open(self.X[idx])
         trans_X = transforms.Compose(self.transform)(X)
-        y = self.y[idx]
+         
+        y = self.Y[idx]
         return trans_X,y 
     
     #각 폴더에 있는 이미지들의 경로 생성 후 Class id 까지
     def getXY(self,root_path):
-        X = []
-        Y = []
-        f = open(root_path + "train_info.csv")
+        x = []
+        y = []
+        csv_path = os.path.join(root_path , "train_info.csv")
+        f = open(csv_path)
         reader = csv.reader(f)
+        next(iter(reader))
         for line in reader:
-            print(line)
-            X.append(line[0])
-            Y.append(line[1])
+            x.append(line[0])
+            y.append(line[1])
             
-            
-        
-        return X,Y
+        return x,y
         
         
 TRAIN_IMG_DIR = "/opt/ml/input/data/train/"
 dataset = IC_Dataset(TRAIN_IMG_DIR)
-print(dataset)
+
+dataloader = data.DataLoader(dataset,batch_size=64)
+train_features, train_labels = next(iter(dataloader))
+print(train_labels[0])
