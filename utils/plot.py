c1afa76d4f9f905ac9fff8a9a5110b9bd22ee094
jjjuuuun <kjy961226@naver.com>
2023-04-13 05:00:20 +0000

    confusion matrix - normalize 수정

--- utils/plot.py
+++ utils/plot.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def plot_confusion_matrix(cm, num_classes, normalize=False, save_path=None):
     if normalize:
-        n_total = torch.sum(cm).item()
+        n_total = torch.sum(cm, 1).view(num_classes, 1)
         np_cm = cm / n_total
         np_cm = np_cm.numpy()
         ax = sns.heatmap(np_cm, annot=True, cmap='Blues', linewidth=.5,
