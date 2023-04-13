ef9610ac0cde94ea0a83775b8c2ce8b552ea195f
Junha <jh58power@naver.com>
2023-04-13 02:50:07 +0000

    wandb 포맷 수정

--- model/model_finetune.py
+++ model/model_finetune.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 # crop 한 이미지 사이즈와 output 클래스 개수 
 def fineTune(model,model_name,class_num):
-    if model_name == "Resnet18":
+    if model_name == "resnet18":
         model.fc = nn.Linear(model.fc.in_features,class_num)
         
         return model
--- train.py
+++ train.py
@@ -23,8 +23,8 @@ print(f"Device >> {device}")
 
 seed = torch.initial_seed()
 
-NUM_EPOCHS = 100
-BATCH_SIZE = 128
+NUM_EPOCHS = 80
+BATCH_SIZE = 64
 MOMENTUM = 0.9
 LR_DECAY = 0.0005
 LR_INIT = 0.01
@@ -34,9 +34,11 @@ NUM_CLASSES = 18
 DEVICE_IDS = [0]
 TRAIN_IMG_DIR = "/opt/ml/input/data/train/"
 OUT_PATH = "./level1_imageclassification-cv-04/data_out/model_save/"
-MODEL = "Resnet18"
+MODEL = "resnet18"
 
-wandb.init(name = "Temp",
+Test_name= f"exp1_bs{BATCH_SIZE}_ep{NUM_EPOCHS}_adam_lr{LR}_{MODEL}.jh"
+
+wandb.init(name = Test_name,
             project = 'Image_classification_mask',
             entity = 'connect-cv-04',
             )
@@ -58,6 +60,8 @@ dataset = IC_Dataset(TRAIN_IMG_DIR,transform_list)
 
 train_dataset , val_dataset = random_split(dataset, [int(len(dataset)*0.8), int(len(dataset)*0.2)])
 
+if (not os.path.isdir(OUT_PATH + Test_name)):
+	os.makedirs(OUT_PATH + Test_name)
 
 print('Dataset created')
 
@@ -108,37 +112,34 @@ for epoch in range(NUM_EPOCHS):
         _, preds = torch.max(output, 1)
         accuracy = torch.sum(preds == classes)
 
-        if total_steps % 10 == 0:
-            # Validation
-            model.eval()
-            for v_imgs, v_classes in val_dataloader:
-                v_imgs, v_classes = v_imgs.to(device), v_classes.to(device).long()
-                with torch.no_grad():
-                    val_out = model(v_imgs)
-                    
-                val_loss = criterion(val_out, v_classes)
-                _, val_preds = torch.max(val_out, 1)
-                val_accuracy = torch.sum(val_preds == v_classes)
-                
-            print('Epoch: {} \tStep: {} \tTrain_Loss: {:.4f} \tTrain_Acc: {} \tVal_Loss: {:.4f} \tVal_Acc: {}'
-                .format(epoch + 1, total_steps, loss.item(), accuracy.item()/BATCH_SIZE,
-                        val_loss.item(), val_accuracy.item()/BATCH_SIZE))
-            wandb.log({
-                "train Accuracy": 100. * accuracy.item()/BATCH_SIZE,
-                "train Loss": loss.item(),
-                "validation Accuracy": 100. * val_accuracy.item()/BATCH_SIZE,
-                "validation Loss": val_loss.item()})
-                
-
+        
+    model.eval()
+    for v_imgs, v_classes in val_dataloader:
+        v_imgs, v_classes = v_imgs.to(device), v_classes.to(device).long()
+        with torch.no_grad():
+            val_out = model(v_imgs)
             
-        total_steps += 1
+        val_loss = criterion(val_out, v_classes)
+        _, val_preds = torch.max(val_out, 1)
+        val_accuracy = torch.sum(val_preds == v_classes)
+        
+    print('Epoch: {} \tStep: {} \tTrain_Loss: {:.4f} \tTrain_Acc: {} \tVal_Loss: {:.4f} \tVal_Acc: {}'
+        .format(epoch + 1, total_steps, loss.item(), accuracy.item()/BATCH_SIZE,
+                val_loss.item(), val_accuracy.item()/BATCH_SIZE))
+    
+    wandb.log({
+        "Train Acc": 100. * accuracy.item()/BATCH_SIZE,
+        "Train Loss": loss.item(),
+        "Val Acc": 100. * val_accuracy.item()/BATCH_SIZE,
+        "Val Loss": val_loss.item()})
+                
+            
+    total_steps += 1
+    lr_scheduler.step()
         
     ################### Wandb 로 로그 기록 ##################
-    wandb.log({
-        "train Accuracy": 100. * accuracy.item()/BATCH_SIZE,
-        "train Loss": loss.item()})
 
     if epoch % 5 == 0:
-        torch.save(model.state_dict(), OUT_PATH + f"resnet18_{epoch:02}.pt")
+        path = os.path.join(OUT_PATH + Test_name + f"{MODEL}_{epoch:02}.pt")
+        torch.save(model.state_dict(), path)
     
-    lr_scheduler.step()
