284763cae4611695cbfaf19add74f0595c9d8c3b
zionia4758 <zionia4758@gmail.com>
2023-04-14 09:02:39 +0000

    dataset_split구현

--- utils/sampler.py
+++ utils/sampler.py
@@ -1,6 +1,9 @@
 import torch
 from torch.utils.data import WeightedRandomSampler
-
+from torch.utils.data import Dataset, DataLoader,Subset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
 def weighted_sampler(dataset, num_classes):
     class_counts = [0] * num_classes
@@ -17,3 +20,13 @@ def weighted_sampler(dataset, num_classes):
     sampler = WeightedRandomSampler(torch.FloatTensor(weights), num_samples)
 
     return sampler
+
+def train_valid_split_by_sklearn(dataset):
+    train_idx, val_idx = train_test_split(np.arange(len(dataset))
+                                      ,train_size=0.8
+                                      ,stratify=dataset.df['ans']
+                                      ,random_state=223
+                                      )
+    train_set = Subset(dataset,train_idx)
+    val_set = Subset(dataset,val_idx)
+    return train_set,val_set
