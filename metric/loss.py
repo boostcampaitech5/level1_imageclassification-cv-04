33ff05b60b6464842298086d4044006923ab26c3
zionia4758 <zionia4758@gmail.com>
2023-04-17 13:32:47 +0000

    CELoss weight추가

--- metric/loss.py
+++ metric/loss.py
@@ -1,10 +1,11 @@
 import torch
 
 def CustomLoss(logit, label):
-    class_loss = torch.nn.CrossEntropyLoss()
+    mask_loss = torch.nn.CrossEntropyLoss(weight=torch.Tensor([1,5,3]))
+    gender_loss = torch.nn.CrossEntropyLoss()
     age_loss = torch.nn.MSELoss()
-    loss = [class_loss(logit[:,:2],label[:,0]),
+    loss = [gender_loss(logit[:,:2],label[:,0]),
             age_loss(logit[:,2:3].float(),label[:,1:2].float()),
-            class_loss(logit[:,3:],label[:,2])]
+            mask_loss(logit[:,3:],label[:,2])]
 
     return loss
