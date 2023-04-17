1f97cae9bbd01b3cd7d7547d138c2e06a26ebdae
zionia4758 <zionia4758@gmail.com>
2023-04-17 08:01:10 +0000

    test_age_average

--- metric/loss.py
+++ metric/loss.py
@@ -3,8 +3,6 @@ import torch
 def CustomLoss(logit, label):
     class_loss = torch.nn.CrossEntropyLoss()
     age_loss = torch.nn.MSELoss()
-    print(logit[0],label[0])
-    print(logit[:5,2:3],label[:5,1:2])
     loss = [class_loss(logit[:,:2],label[:,0]),
             age_loss(logit[:,2:3].float(),label[:,1:2].float()),
             class_loss(logit[:,3:],label[:,2])]
