273b9aad1b57d313ac483d58f36ded54b8bc9389
zionia4758 <zionia4758@gmail.com>
2023-04-25 03:28:26 +0000

    transform 임시 개편(추후 양식 지정 필요)

--- utils/transform.py
+++ utils/transform.py
@@ -6,38 +6,19 @@ def get_transform(args):
     with open(args.transform_path, 'r') as f:
         transform_json = f.read()
     tf_list = json.loads(transform_json)
+    
+   
+    transform_dict = {'resize':'Resize', "totensor":'ToTensor','centercrop' : "CenterCrop",'colorjitter' : "ColorJitter",
+                      'randomhorizontalflip':'RandomHorizontalFlip','randomrotation' : "RandomRotation",'gaussianblur' : "GaussianBlur",
+                      'randomaffine': "RandomAffine",'normalize':'Normalize'}
 
-    statistic = {'imagenet' : {'mean' : (0.485, 0.456, 0.406),
-                               'std' : (0.229, 0.224, 0.225)},
-                 'mask' : {'mean' : (0.5601, 0.5241, 0.5014),
-                           'std' : (0.2331, 0.2430, 0.2456)}}
-    
-    transform_dict = {'resize' : transforms.Resize((tf_list['resize']['img_height'], 
-                                                    tf_list['resize']['img_width'])),
-                      'totensor' : transforms.ToTensor(),
-                      'normalize' : transforms.Normalize(mean=statistic[tf_list['normalize']['normalize_statistic']]['mean'],
-                                                         std=statistic[tf_list['normalize']['normalize_statistic']]['std']),
-                      'centercrop' : transforms.CenterCrop((tf_list['centercrop']['img_height'], 
-                                                            tf_list['centercrop']['img_width'])),
-                      'colorjitter' : transforms.ColorJitter(tf_list['colorjitter']['brightness'], 
-                                                             tf_list['colorjitter']['contrast'], 
-                                                             tf_list['colorjitter']['saturation'], 
-                                                             tf_list['colorjitter']['hue']),
-                      'randomhorizontalflip' : transforms.RandomHorizontalFlip(tf_list['randomhorizontalflip']['flip_prob']),
-                      'randomrotation' : transforms.RandomRotation(tf_list['randomrotation']['degrees']),
-                      'gaussianblur' : transforms.GaussianBlur(tf_list['gaussianblur']['kernel_size'],
-                                                               (tf_list['gaussianblur']['sigma_min'], tf_list['gaussianblur']['sigma_max'])),
-                      'randomaffine': transforms.RandomAffine(degrees=tf_list['randomaffine']['degrees'],
-                                                              shear=tuple(tf_list['randomaffine']['shear']),
-                                                              translate=tuple(tf_list['randomaffine']['translate']))}
-    
-    list_ = []
+    transform_list = []
     config = {}
     for key in args.transform_list:
-        list_.append(transform_dict[key])
+        transform_list.append(getattr(transforms,transform_dict[key])(**tf_list[key]))
         config[key] = tf_list[key]
     
-    transform = transforms.Compose(list_)
+    transform = transforms.Compose(transform_list)
 
     return transform, config
     
